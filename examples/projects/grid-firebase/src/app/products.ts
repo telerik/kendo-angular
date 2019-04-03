f3977f0636ef4a6985a40783ec8909c7e9251119
dtopalov <dimiter.topalov79@gmail.com>
2019-04-03 16:07:41 +0300

    feat: add Grid Firebase example

---
+++ examples/projects/grid-firebase/src/app/products.ts
@@ -0,0 +1,74 @@
+export const products = {
+    "products": 
+        {"-LVIsWcwjfKEJDJy-C8S": {
+            "key": "-LVIsWcwjfKEJDJy-C8S",
+            "ProductName": "Chai",
+            "UnitPrice": 18,
+            "UnitsInStock": 39,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-C8F": {
+            "key": "-LVIsWcwjfKEJDJy-C8F",
+            "ProductName": "Chang",
+            "UnitPrice": 19,
+            "UnitsInStock": 17,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-C81": {
+            "key": "-LVIsWcwjfKEJDJy-C81",
+            "ProductName": "Aniseed Syrup",
+            "UnitPrice": 10,
+            "UnitsInStock": 13,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-C82": {
+            "key": "-LVIsWcwjfKEJDJy-C82",
+            "ProductName": "Chef Anton's Cajun Seasoning",
+            "UnitPrice": 22,
+            "UnitsInStock": 53,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-B82": {
+            "key": "-LVIsWcwjfKEJDJy-B82",
+            "ProductName": "Chef Anton's Gumbo Mix",
+            "UnitPrice": 21.35,
+            "UnitsInStock": 0,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-C42": {
+            "key": "-LVIsWcwjfKEJDJy-C42",
+            "ProductName": "Grandma's Boysenberry Spread",
+            "UnitPrice": 25,
+            "UnitsInStock": 120,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-C52": {
+            "key": "-LVIsWcwjfKEJDJy-C52",
+            "ProductName": "Uncle Bob's Organic Dried Pears",
+            "UnitPrice": 30,
+            "UnitsInStock": 15,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-G82": {
+            "key": "-LVIsWcwjfKEJDJy-G82",
+            "ProductName": "Northwoods Cranberry Sauce",
+            "UnitPrice": 40,
+            "UnitsInStock": 6,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEFDJy-G82": {
+            "key": "-LVIsWcwjfKEFDJy-G82",
+            "ProductName": "Mishi Kobe Niku",
+            "UnitPrice": 97,
+            "UnitsInStock": 29,
+            "Discontinued": false
+        },
+        "-LVIsWcwjfKEJDJy-P82": {
+            "key": "-LVIsWcwjfKEJDJy-P82",
+            "ProductName": "Ikura",
+            "UnitPrice": 31,
+            "UnitsInStock": 31,
+            "Discontinued": false
+        }
+    }
+}
