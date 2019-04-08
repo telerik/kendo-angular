6569cac77e72458e907fcb2e5e6b1d5de982c085
dtopalov <dimiter.topalov79@gmail.com>
2019-04-08 16:32:57 +0300

    chore: add universal samples, move gitignore to root

---
+++ examples-standalone/angular-universal/node_sample_application/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TransferHttpCacheModule} from '@nguniversal/common';
+import { GridModule } from '@progress/kendo-angular-grid';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({appId: 'my-app'}),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full'},
+      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
+    ]),
+    TransferHttpCacheModule,
+    BrowserAnimationsModule,
+    GridModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
