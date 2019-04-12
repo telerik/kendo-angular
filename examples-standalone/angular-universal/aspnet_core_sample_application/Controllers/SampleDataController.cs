eab9fcc0932a059bd96023bf479108c49c9b8313
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-04-12 19:27:50 +0300

    chore: upgrade Universal demo to .NET Core 2.2

--- examples-standalone/angular-universal/aspnet_core_sample_application/Controllers/SampleDataController.cs
+++ examples-standalone/angular-universal/aspnet_core_sample_application/Controllers/SampleDataController.cs
@@ -20,7 +20,9 @@ namespace dotnet_angular.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
+                // Use the "Round-trip Format Specifier" for dates
+                // https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings#the-round-trip-o-o-format-specifier
+                DateFormatted = DateTime.Now.AddDays(index).ToString("o"),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             });
