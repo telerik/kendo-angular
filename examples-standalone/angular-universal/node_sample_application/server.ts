fa2c3b574ccaa7a9d4278ef6ba8f51aa4f3a3537
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-04-19 16:11:36 +0300

    chore: upgrade Angular Universal Express sample app

--- examples-standalone/angular-universal/node_sample_application/server.ts
+++ examples-standalone/angular-universal/node_sample_application/server.ts
@@ -1,10 +1,13 @@
 import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
+
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
-import {join} from 'path';
-import {readFileSync} from 'fs';
+import { join } from 'path';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -15,16 +18,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
-// Our index.html we'll use as our template
-const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
-
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main.bundle');
-
-// Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -37,16 +32,15 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-/* - Example Express Rest API endpoints -
-  app.get('/api/**', (req, res) => { });
-*/
+// Example Express Rest API endpoints
+// app.get('/api/**', (req, res) => { });
 
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
   maxAge: '1y'
 }));
 
-// ALl regular routes use the Universal engine
+// All regular routes use the Universal engine
 app.get('*', (req, res) => {
   res.render('index', { req });
 });
