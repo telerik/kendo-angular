21df6f352c88f0c37aa0a8a95f42755190d04121
Elena Gancheva <elena.gancheva@telerik.com>
2019-04-23 12:45:19 +0300

    chore: migrate dashboard example to latest

--- examples-standalone/electron-dashboard/main.ts
+++ examples-standalone/electron-dashboard/main.ts
@@ -58,7 +58,7 @@ function createWindow() {
     }));
   }
 
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
