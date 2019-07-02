aadfbd1cc51f77ef2936ede5fadd4b9065401bbd
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-07-02 09:08:51 +0300

    refactor: legacy HttpModule

--- examples/projects/integration-i18n/src/app/app.component.ts
+++ examples/projects/integration-i18n/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Response, ResponseOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
--- examples/projects/integration-pwa/src/app/shared/github.service.ts
+++ examples/projects/integration-pwa/src/app/shared/github.service.ts
@@ -1,47 +1,50 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { forkJoin } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 const baseUrl = 'https://api.github.com/repos/telerik/kendo-ui-core/issues';
+const token = 'b95116792cba5a8169a1ec10640d8c16535c6419';
+const epoch = '2018-01-01T00:00:00';
+
+const headers = new HttpHeaders({
+  // Generate your own token through
+  // https://github.com/settings/tokens
+
+  Authorization: `token ${token}`
+});
 
 @Injectable()
 export class GithubService {
-    private ghToken = 'Yjk1MTE2NzkyY2JhNWE4MTY5YTFlYzEwNjQwZDhjMTY1MzVjNjQxOQ==';
-    private headers = new HttpHeaders({
-        // Generate your own token through
-        // https://github.com/settings/tokens
-
-        'Authorization': "token b95116792cba5a8169a1ec10640d8c16535c6419"
-    });
     constructor(public http: HttpClient) { }
 
     getGithubIssues(pages) {
         return forkJoin(this.getIssuesUrls(pages));
     }
 
+    getTotalIssuesCount() {
+        return this.http.get(`${baseUrl}?since='${epoch}'`, { headers });
+    }
+
     getIssuesUrls({ pages }) {
         const result = [];
         for (let index = 1; index < pages; index++) {
             result.push(
-                this.http.get(`${baseUrl}?state=all&page=${index}&per_page=100`, { headers: this.headers })
-                    .pipe(map(this.handleResponse))
+                this.http.get(`${baseUrl}?state=all&page=${index}&per_page=100`, { headers })
             );
         }
         return result;
     }
 
-    getGithubUser(username) {
-        return this.http.get(`https://api.github.com/users/${username}`, { headers: this.headers })
-            .pipe(map(this.handleResponse));
+    getIssuesPerPage(skip: number, take: number) {
+        const page = (take + skip) / take;
+        return this.http.get(`${baseUrl}?state=all&page=${page}&per_page=${take}`, { headers });
+    }
+
+    getGithubUser(username: string) {
+        return this.http.get(`https://api.github.com/users/${username}`, { headers });
     }
 
     getGithubIssue(id: number) {
-        return this.http.get(`${baseUrl}/${id}`, { headers: this.headers })
-            .pipe(map(this.handleResponse));
-    }
-
-    handleResponse(res: Response): any {
-        return res;
+        return this.http.get(`${baseUrl}/${id}`, { headers });
     }
 }
