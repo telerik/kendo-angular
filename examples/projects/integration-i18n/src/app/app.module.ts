868c8f514877311a9f310b0c45b15b3532ac6b52
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-07-02 09:08:51 +0300

    refactor: legacy HttpModule

--- examples/projects/integration-i18n/src/app/app.module.ts
+++ examples/projects/integration-i18n/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 import { GridModule } from '@progress/kendo-angular-grid';
 
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     GridModule
   ],
   bootstrap: [AppComponent]
