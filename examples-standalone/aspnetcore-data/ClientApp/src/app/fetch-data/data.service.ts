a317913f7643f5a64eb9164ca9aacf6a9f87445e
Svetlin Kozhuharov <kozhuhar@progress.com>
2019-10-10 17:23:47 +0300

    chore: update the project to demonstrate CRUD operations performed server-side (#2481)

--- examples-standalone/aspnetcore-data/ClientApp/src/app/fetch-data/data.service.ts
+++ examples-standalone/aspnetcore-data/ClientApp/src/app/fetch-data/data.service.ts
@@ -1,32 +1,94 @@
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, Inject } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
 import { GridDataResult } from '@progress/kendo-angular-grid';
 import {
-  DataSourceRequestState,
-  toDataSourceRequestString,
-  translateDataSourceResultGroups
+    State,
+    toDataSourceRequestString,
+    translateDataSourceResultGroups,
 } from '@progress/kendo-data-query';
 
 @Injectable()
-export class DataService {
-    constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {}
+export class DataService extends BehaviorSubject<any[]> {
 
-    public fetch(state: DataSourceRequestState): Observable<any> {
-        const queryStr = `${toDataSourceRequestString(state)}`;
-        const hasGroups = state.group && state.group.length;
+    constructor(private http: HttpClient) {
+        super([]);
+    }
 
-        return this.http
-            // Send the state to the server
-            .get(`${this.baseUrl}api/Products?${queryStr}`).pipe(
-              // Process the response
-              map(({ data, total }: GridDataResult): GridDataResult => {
-                return {
-                    data: hasGroups ? translateDataSourceResultGroups(data) : data,
-                    total: total
+    private BASE_URL = 'api/Blogs';
+    private data: any[] = [];
+    public state: State = {
+        skip: 0,
+        take: 5,
+        filter: { filters: [], logic: 'or' },
+        group: [],
+        sort: []
+    };
+
+    public read() {
+        if (this.data.length) {
+            return super.next(this.data);
+        }
+
+        this.fetch()
+            .pipe(
+                tap(data => {
+                    this.data = data;
+                })
+            )
+            .subscribe(data => {
+                super.next(data);
+            });
+    }
+
+
+    public fetch(dataItem?: any, action: string = ''): Observable<any> {
+
+        switch (action) {
+            case '': {
+                const queryStr = `${toDataSourceRequestString(this.state)}`;
+                const hasGroups = this.state.group && this.state.group.length;
+
+                return this.http.get(`${this.BASE_URL}?${queryStr}`).pipe(
+                    // Process the response
+                    map(({ data, total }: GridDataResult): GridDataResult => {
+                        return {
+                            data: hasGroups ? translateDataSourceResultGroups(data) : data,
+                            total: total
+                        };
+                    }
+                    ));
+            }
+            case 'create': {
+                return this.http.post(`${this.BASE_URL}`, dataItem);
+            }
+            case 'edit': {
+                return this.http.put(`${this.BASE_URL}/${dataItem.blogId}`, dataItem);
+            }
+            case 'delete': {
+                const options = {
+                    headers: {},
+                    body: dataItem,
                 };
-              }
-            ));
+
+                return this.http.delete(`${this.BASE_URL}/${dataItem.blogId}`, options);
+            }
+        }
+    }
+
+    public save(dataItem: any, isNew?: boolean) {
+        if (isNew) {
+            const newBlog = { Url: dataItem.url };
+            this.fetch(newBlog, 'create').subscribe(() => this.read(), () => this.read());
+        } else {
+            this.fetch(dataItem, 'edit').subscribe(() => this.read(), () => this.read());
+        }
+    }
+
+    public delete(dataItem: any) {
+        this.fetch(dataItem, 'delete').subscribe(() => this.read(), () => this.read());
     }
 }
