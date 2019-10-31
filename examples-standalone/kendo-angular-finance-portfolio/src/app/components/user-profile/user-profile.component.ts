382d6dbc0e515772d3c63bb17cdcdf71b8a8302b
Elena Gancheva <elena.gancheva@telerik.com>
2019-10-31 14:57:18 +0200

    feat: add user portfolio

--- examples-standalone/kendo-angular-finance-portfolio/src/app/components/user-profile/user-profile.component.ts
+++ examples-standalone/kendo-angular-finance-portfolio/src/app/components/user-profile/user-profile.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { StockDataService } from 'src/app/services/stock-data.service';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Stock } from 'src/app/models/stock';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent {
+    public chartData: any[];
+    public gridData: any[];
+    public seriesLabels = {
+        visible: true,
+        content: (args: any) => `${args.category}\n${args.value}`
+    };
+    private serviceSubscription: any;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(public service: StockDataService) {
+        this.serviceSubscription = service.getDataStream().subscribe(data => {
+            this.chartData = data.data.map((item: Stock) => ({
+                category: item.symbol,
+                value: item.price
+            }));
+            this.gridData = data.data.map((item: Stock) => ({
+                symbol: item.symbol,
+                name: item.name,
+                price: item.price
+            }));
+        });
+    }
 }
