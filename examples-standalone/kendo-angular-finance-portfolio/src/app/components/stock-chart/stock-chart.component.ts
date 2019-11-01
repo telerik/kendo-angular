f137502a7833e2737fbf035183d90affc076bebe
Elena Gancheva <elena.gancheva@telerik.com>
2019-11-01 13:12:49 +0200

    feat: add active time filter items

--- examples-standalone/kendo-angular-finance-portfolio/src/app/components/stock-chart/stock-chart.component.ts
+++ examples-standalone/kendo-angular-finance-portfolio/src/app/components/stock-chart/stock-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-stock-chart',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./stock-chart.component.scss']
 })
 export class StockChartComponent {
-    public intervals: Array<string> = ['5M', '15M', '30M', '1H', '4H', '1D', '1W'];
     public range = { start: null, end: null };
+
+    public timeFilters: Array<string> = ['1H', '4H', '12H', '1D', '4D', '1W'];
+
+    public intervals: Array<string> = ['5M', '15M', '30M', '1H', '4H', '1D', '1W'];
     public selectedInterval = '1H';
 
     public selectedChart = 'Candle';
     public charts: Array<string> = ['Candle', 'Line', 'Area'];
+
+    private activeTimeFilter = '1H';
+
+    public onTimeFilterClick(filter: string): void {
+        if (this.activeTimeFilter === filter) { return; }
+        this.activeTimeFilter = filter;
+    }
 }
