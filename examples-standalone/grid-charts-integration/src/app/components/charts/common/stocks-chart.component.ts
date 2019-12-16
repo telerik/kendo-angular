10ee7d5e9ef0084a0af3d5bc57830d41d4827c76
Ivo Valkov <ivos.valkov@gmail.com>
2019-12-16 18:27:45 +0200

    chore: address comments

--- examples-standalone/grid-charts-integration/src/app/components/charts/common/stocks-chart.component.ts
+++ examples-standalone/grid-charts-integration/src/app/components/charts/common/stocks-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { series, seriesTypes } from "../../../data";
 import { Stock, ChartConfig } from "../../../model";
 import { getChartStack, getTitle, getChartType } from "../../../utils";
