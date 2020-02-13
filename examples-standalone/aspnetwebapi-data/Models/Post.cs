25f1d934633f67c77dcd70ff3deb49fd2204c9b6
Svetlin Kozhuharov <kozhuhar@progress.com>
2020-02-13 15:06:17 +0200

    docs: rename project

---
+++ examples-standalone/aspnetwebapi-data/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aspnetwebapi_data
+{
+    public partial class Post
+    {
+        public int PostId { get; set; }
+        public int BlogId { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+
+        public virtual Blog Blog { get; set; }
+    }
+}
