d9fd797a6fb2821867d8af6808b2b94c98727598
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2020-02-18 09:02:11 +0200

    refactor(angular-universal): upgrade to Angular 9.x

--- examples-standalone/angular-universal/node_sample_application/src/main.ts
+++ examples-standalone/angular-universal/node_sample_application/src/main.ts
@@ -4,13 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Required by Kendo UI for Angular Charts
-import 'hammerjs';
-
 if (environment.production) {
   enableProdMode();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule);
+  platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
 });
--- examples-standalone/angular-universal/node_sample_application/static.paths.ts
+++
@@ -1,5 +0,0 @@
-export const ROUTES = [
-  '/',
-  '/lazy',
-  '/lazy/nested'
-];
