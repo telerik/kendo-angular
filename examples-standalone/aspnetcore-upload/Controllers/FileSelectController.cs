ecf7736265d01763861475b3dc0a57c01788bade
Martin Bechev <martinbechev@gmail.com>
2020-03-17 17:56:22 +0200

    address comments, add chunk and fileselect controllers

---
+++ examples-standalone/aspnetcore-upload/Controllers/FileSelectController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore_upload.Controllers
+{
+    public interface IForm
+    {
+        string username { get; set; }
+        string avatar { get; set; }
+    }
+    public class FileSelectController : Controller
+    {
+        [Route("api/Submit/Form")]
+        [HttpPost]
+        public ActionResult OnSubmit(IForm form)
+        {
+            // Return an empty string to signify success
+            return Content("");
+        }
+    }
+}
