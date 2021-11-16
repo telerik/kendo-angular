5389a6be941b8a97163e58fdf7b1e9d87ee6eb18
Svetlin Kozhuharov <kozhuhar@progress.com>
2021-11-16 15:38:51 +0200

    chore: update coffee warehouse to Angular 12

--- examples-standalone/coffee-warehouse/src/polyfills.ts
+++ examples-standalone/coffee-warehouse/src/polyfills.ts
@@ -55,7 +55,7 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
