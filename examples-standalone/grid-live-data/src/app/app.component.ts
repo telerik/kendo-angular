f5238cbca4b0b9760ac96eac3f91c19d6512f8a6
yanmariomenev <yanmario.menev@gmail.com>
2021-12-10 14:12:50 +0200

    add price animation

--- examples-standalone/grid-live-data/src/app/app.component.ts
+++ examples-standalone/grid-live-data/src/app/app.component.ts
@@ -2,12 +2,29 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { RowClassArgs } from '@progress/kendo-angular-grid';
 import { Observable } from 'rxjs';
 import { Stock, StocksService } from './services/stocks.service';
+import { trigger, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    animations: [
+        trigger('positiveState', [
+            transition('void => *', []),
+            transition('* => void', []),
+            transition('* => *', [
+                animate(1600, keyframes([style({ color: '#32CD32', offset: 0.0 }), style({ color: 'inherit', offset: 1.0 })]))
+            ])
+        ]),
+        trigger('negativeState', [
+            transition('void => *', []),
+            transition('* => void', []),
+            transition('* => *', [
+                animate(1600, keyframes([style({ color: '#FF0000', offset: 0.0 }), style({ color: 'inherit', offset: 1.0 })]))
+            ])
+        ])
+    ]
 })
 export class AppComponent {
     public gridData: Observable<Stock[]>;
