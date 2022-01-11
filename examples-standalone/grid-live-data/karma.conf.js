16c5ff830a32f18361fb66da5df11ed5b26bdd00
Preslava Petrova <preslavapetrova1@gmail.com>
2022-01-11 15:40:07 +0200

    rename demo

--- examples-standalone/grid-live-data/karma.conf.js
+++ examples-standalone/grid-live-data/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/grid-live-demo'),
+      dir: require('path').join(__dirname, './coverage/grid-live-data'),
       subdir: '.',
       reporters: [
         { type: 'html' },
