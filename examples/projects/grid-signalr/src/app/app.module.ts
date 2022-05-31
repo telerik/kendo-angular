0c56ab41eee46345e70a5bfb2b4970cc456854e4
Svetlin Kozhuharov <kozhuhar@progress.com>
2022-05-31 11:15:02 +0300

    chore: update grid-signalr

--- examples/projects/grid-signalr/src/app/app.module.ts
+++ examples/projects/grid-signalr/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, GridModule, BrowserAnimationsModule],
+  imports: [BrowserModule, HttpClientModule, InputsModule, ButtonsModule, ReactiveFormsModule, GridModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
