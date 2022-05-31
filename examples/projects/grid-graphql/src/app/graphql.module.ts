99843d964ce7db8595e0eeaf3654c3532e7f8828
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2022-05-31 17:36:46 +0300

    refactor: lint

--- examples/projects/grid-graphql/src/app/graphql.module.ts
+++ examples/projects/grid-graphql/src/app/graphql.module.ts
@@ -4,20 +4,22 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLink} from 'apollo-angular/http';
 import {InMemoryCache} from '@apollo/client/core';
 
+const SERVER_URI = 'http://localhost:4000/graphql'; // <-- add the URL of the GraphQL server here
+
 @NgModule({
   exports: [ApolloModule, HttpClientModule],
-  providers: [
+  providers: [{
     provide: APOLLO_OPTIONS,
     useFactory: (httpLink: HttpLink) => {
       return {
         cache: new InMemoryCache(),
         link: httpLink.create({
-          uri: SERVER_URI,
-        }),
+          uri: SERVER_URI
+        })
       };
     },
     deps: [HttpLink],
-  ],
+  }],
 })
 export class GraphQLModule {}
 
