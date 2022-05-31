cdb9e5b438844788b961e1c96a1300c083cabe5f
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2022-05-31 13:37:02 +0300

    fix: GraphQL demo

--- examples/projects/grid-graphql/src/app/graphql.module.ts
+++ examples/projects/grid-graphql/src/app/graphql.module.ts
@@ -1,24 +1,22 @@
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-
-const uri = 'http://localhost:4000/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink) {
-  return {
-    link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
-  };
-}
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
 
 @NgModule({
-  exports: [ApolloModule, HttpLinkModule],
+  exports: [ApolloModule, HttpClientModule],
   providers: [
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
-      deps: [HttpLink],
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: SERVER_URI,
+        }),
+      };
     },
+    deps: [HttpLink],
   ],
 })
 export class GraphQLModule {}
