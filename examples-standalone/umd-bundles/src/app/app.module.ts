8fa4306eb7c34add1d2d33059cf7d0c8897335a7
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2022-06-08 11:55:59 +0300

    feat: update umd-bundle example

--- examples-standalone/umd-bundles/src/app/app.module.ts
+++ examples-standalone/umd-bundles/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { GridModule } from '@progress/kendo-angular-grid';
+import { ExcelModule, GridModule } from '@progress/kendo-angular-grid';
 
 @NgModule({
     declarations: [
@@ -11,7 +11,8 @@ import { GridModule } from '@progress/kendo-angular-grid';
     imports: [
         BrowserModule,
         ButtonsModule,
-        GridModule
+        GridModule,
+        ExcelModule
     ],
     providers: [],
     bootstrap: [
