4a3e902b416fd09020eb02f7ac3e2604fdfea714
mbechev <martinbechev@gmail.com>
2023-02-03 16:25:51 +0200

    docs: update year in standalone apps

--- examples-standalone/finance-portfolio/src/app/components/footer/footer.component.ts
+++ examples-standalone/finance-portfolio/src/app/components/footer/footer.component.ts
@@ -8,5 +8,6 @@ import { Router } from '@angular/router';
     encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
+    public currentYear: number = new Date().getFullYear();
     constructor(public router: Router) { }
 }
