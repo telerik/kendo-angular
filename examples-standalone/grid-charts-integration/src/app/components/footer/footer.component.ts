a76ff5787c640f88c943b041748a2c1f224f7027
Martin Bechev <33104481+mbechev@users.noreply.github.com>
2023-07-04 17:14:25 +0300

    docs: update year in standalone apps

--- examples-standalone/grid-charts-integration/src/app/components/footer/footer.component.ts
+++ examples-standalone/grid-charts-integration/src/app/components/footer/footer.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+    public currentYear: number = new Date().getFullYear();
 }
