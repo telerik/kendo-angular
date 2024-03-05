4d07c743f198c0b0ba948b33fd7fc9db3471325c
mbechev <martinbechev@gmail.com>
2024-03-05 18:37:56 +0200

    update server

---
+++ examples-standalone/aspnetcore-data/Server/Controllers/BlogsController.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly BloggingContext _context;
+
+        public BlogsController(BloggingContext context)
+        {
+
+            _context = context;
+        }
+
+        [Route("api/Blogs")]
+        [HttpGet]
+        public JsonResult GetProducts([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = Json(this._context.Blog.ToList().ToDataSourceResult(request));
+            return result;
+        }
+
+        [Route("api/Blogs")]
+        [HttpPost]
+        public JsonResult AddBlog([FromBody] Blog request)
+        {
+
+            this._context.Add(request);
+            this._context.SaveChanges();
+            var result = Json(this._context.Blog.Local);
+            return result;
+
+        }
+
+        [Route("api/Blogs/{id}")]
+        [HttpDelete]
+        public JsonResult DeleteBlog([FromBody] Blog request)
+        {
+            this._context.Remove(request);
+            this._context.SaveChanges();
+            var result = Json(this._context.Blog.Local);
+            return result;
+        }
+
+        [Route("api/Blogs/{id}")]
+        [HttpPut]
+
+        public JsonResult Editblog([FromBody] Blog request)
+        {
+
+            var existingblog = this._context.Blog.Where(b => b.BlogId == request.BlogId).FirstOrDefault<Blog>();
+
+            if (existingblog != null)
+            {
+                existingblog.Url = request.Url;
+            }
+            this._context.SaveChanges();
+
+            var result = Json(this._context.Blog.Local);
+            return result;
+        }
+    }
+}
---
+++ examples-standalone/aspnetcore-data/Server/Model/Blog.cs
@@ -0,0 +1,15 @@
+﻿namespace Server.Models
+{
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public virtual ICollection<Post> Post { get; set; }
+    }
+}
