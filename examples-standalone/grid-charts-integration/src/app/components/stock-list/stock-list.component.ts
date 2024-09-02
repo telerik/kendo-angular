b8b23a6dc10eb3923ffced42922aa73c30ba306f
Georgi Slavov <108671673+georgi-sla@users.noreply.github.com>
2024-09-02 10:36:20 +0300

    feat: update column width and enable drag selection (#4394)

--- examples-standalone/grid-charts-integration/src/app/components/stock-list/stock-list.component.ts
+++ examples-standalone/grid-charts-integration/src/app/components/stock-list/stock-list.component.ts
@@ -1,6 +1,16 @@
 import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
-import { ContextMenuComponent, ContextMenuSelectEvent, MenusModule } from "@progress/kendo-angular-menu";
-import { CellClickEvent, ExcelModule, GridComponent, GridModule, SelectableSettings } from "@progress/kendo-angular-grid";
+import {
+  ContextMenuComponent,
+  ContextMenuSelectEvent,
+  MenusModule,
+} from '@progress/kendo-angular-menu';
+import {
+  CellClickEvent,
+  ExcelModule,
+  GridComponent,
+  GridModule,
+  SelectableSettings,
+} from '@progress/kendo-angular-grid';
 import { IconsModule } from '@progress/kendo-angular-icons';
 import { SVGIcon, infoCircleIcon } from '@progress/kendo-svg-icons';
 import { ChartConfig, Stock } from '../../model';
@@ -18,82 +28,96 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-stock-list',
   standalone: true,
-  imports: [MenusModule, GridModule, IconsModule, NumberFormatPipe, DialogsModule, ExcelModule, WindowComponent, DayChartComponent, WindowComponent, CommonModule],
+  imports: [
+    MenusModule,
+    GridModule,
+    IconsModule,
+    NumberFormatPipe,
+    DialogsModule,
+    ExcelModule,
+    WindowComponent,
+    DayChartComponent,
+    WindowComponent,
+    CommonModule,
+  ],
   templateUrl: './stock-list.component.html',
   styleUrl: './stock-list.component.scss',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class StockListComponent {
-    @ViewChild('gridmenu') public gridContextMenu: ContextMenuComponent | undefined;
-    @ViewChild('grid') public grid: GridComponent | undefined;
+  @ViewChild('gridmenu') public gridContextMenu:
+    | ContextMenuComponent
+    | undefined;
+  @ViewChild('grid') public grid: GridComponent | undefined;
 
-    public infoIcon: SVGIcon = infoCircleIcon;
+  public infoIcon: SVGIcon = infoCircleIcon;
 
-    public items: object[] = menuItems;
-    public opened = false;
-    public chartConfiguration: ChartConfig = { seriesType: 'line', stack: false };
-    public gridData: Stock[] = stocksInPortfolio;
-    public selectableSettings: SelectableSettings = {
-        checkboxOnly: false,
-        mode: 'multiple'
+  public items: object[] = menuItems;
+  public opened = false;
+  public chartConfiguration: ChartConfig = { seriesType: 'line', stack: false };
+  public gridData: Stock[] = stocksInPortfolio;
+  public selectableSettings: SelectableSettings = {
+    checkboxOnly: false,
+    mode: 'multiple',
+    drag: true,
+  };
+  public mySelection: Stock[] = stocksInPortfolio.slice(0, 4);
+
+  constructor() {
+    this.allData = this.allData.bind(this);
+  }
+
+  public onCellClick(e: CellClickEvent): void {
+    if (e.type === 'contextmenu') {
+      const originalEvent = e.originalEvent;
+      originalEvent.preventDefault();
+      this.gridContextMenu?.show({
+        left: originalEvent.pageX,
+        top: originalEvent.pageY,
+      });
+    }
+  }
+
+  public selectBy(e: any) {
+    return e.dataItem;
+  }
+
+  public onSelectionChange() {
+    if (this.opened) {
+      setTimeout(() => {
+        this.mySelection = [...this.mySelection];
+      });
+    }
+  }
+
+  public onSelect(e: ContextMenuSelectEvent): void {
+    if (e.item.text === 'Charts' || e.item.items !== undefined) {
+      return;
+    }
+
+    if (e.item.text === 'Export Excel') {
+      this.grid?.saveAsExcel();
+    } else {
+      this.chartConfiguration = {
+        chartName: e.item.text,
+        seriesType: getChartType(e.item.text) as SeriesType,
+        stack: getChartStack(e.item.text),
+      };
+      if (!this.opened) {
+        this.opened = true;
+      }
+      this.gridContextMenu?.hide();
+    }
+  }
+
+  public allData() {
+    const result = {
+      data: this.mySelection,
     };
-    public mySelection: Stock[] = stocksInPortfolio.slice(0, 4);
+    return result;
+  }
 
-    constructor() {
-        this.allData = this.allData.bind(this);
-    }
-
-    public onCellClick(e: CellClickEvent): void {
-        if (e.type === 'contextmenu') {
-            const originalEvent = e.originalEvent;
-            originalEvent.preventDefault();
-            this.gridContextMenu?.show({
-                left: originalEvent.pageX,
-                top: originalEvent.pageY
-            });
-        }
-    }
-
-    public selectBy(e: any) {
-        return e.dataItem;
-    }
-
-    public onSelectionChange() {
-        if (this.opened) {
-            setTimeout(() => {
-                this.mySelection = [...this.mySelection];
-            });
-        }
-    }
-
-    public onSelect(e: ContextMenuSelectEvent): void {
-        if (e.item.text === 'Charts' || e.item.items !== undefined) {
-            return;
-        }
-
-        if (e.item.text === 'Export Excel') {
-            this.grid?.saveAsExcel();
-        } else {
-            this.chartConfiguration = {
-                chartName: e.item.text,
-                seriesType: getChartType(e.item.text) as SeriesType,
-                stack: getChartStack(e.item.text)
-            };
-            if (!this.opened) {
-                this.opened = true;
-            }
-            this.gridContextMenu?.hide();
-        }
-    }
-
-    public allData() {
-        const result = {
-            data: this.mySelection,
-        };
-        return result;
-    }
-
-    public close() {
-        this.opened = false;
-    }
+  public close() {
+    this.opened = false;
+  }
 }
