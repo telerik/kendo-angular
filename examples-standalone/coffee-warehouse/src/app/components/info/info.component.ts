365b9cf1a921e4fef944ce702b3d8387935d97fd
Slavena Ilieva <slilieva@progress.com>
2024-09-20 18:40:01 +0300

    chore: pause video when pauseAnimations true

--- examples-standalone/coffee-warehouse/src/app/components/info/info.component.ts
+++ examples-standalone/coffee-warehouse/src/app/components/info/info.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { MessageService } from '@progress/kendo-angular-l10n';
 import { CustomMessagesService } from '../../services/custom-messages.service';
+import { SettingsService } from '../../settings.service';
+import { isPresent } from '@progress/kendo-angular-common';
 
 @Component({
     selector: 'app-info-component',
     templateUrl: './info.component.html'
 })
-export class InfoComponent {
+export class InfoComponent implements AfterViewInit {
     public customMsgService: CustomMessagesService;
+    public set pauseAnimation(value: boolean) {
+        this._pauseAnimation = value;
 
-    constructor(public msgService: MessageService) {
+        if (!isPresent(this.videoElement)) { return; }
+        value ? this.videoElement.pause() : this.videoElement.play();
+    }
+
+    public ngAfterViewInit(): void {
+        this.videoElement = document.getElementById('video') as HTMLVideoElement;
+    }
+
+    private _pauseAnimation = false;
+    private videoElement: HTMLVideoElement;
+
+    constructor(public msgService: MessageService, private settingsService: SettingsService) {
         this.customMsgService = this.msgService as CustomMessagesService;
+
+        this.settingsService.changes.subscribe(settings => {
+            if (settings.pauseAnimations !== this._pauseAnimation) {
+                this.pauseAnimation = settings.pauseAnimations;
+            }
+        });
     }
 }
