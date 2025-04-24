0415e63bb534bdc3db9b02901179bbe017caacf3
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2025-04-24 15:53:58 +0300

    chore: add scheduler

--- examples-standalone/kendoangular-landing-page/src/app/app.component.ts
+++ examples-standalone/kendoangular-landing-page/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { BottomLeftComponent } from './components/bottom-left/bottom-left.component';
 import { TransactionsDashboardComponent } from './components/transactions-dashboard/transactions-dashboard.component';
 import { BottomRightComponent } from './components/bottom-right/bottom-right.component';
+import { SchedulerComponent } from "./components/scheduler/scheduler.component";
 
 @Component({
   selector: 'app-root',
   encapsulation: ViewEncapsulation.None,
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, BottomLeftComponent,BottomRightComponent, TransactionsDashboardComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, BottomLeftComponent, BottomRightComponent, TransactionsDashboardComponent, SchedulerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
