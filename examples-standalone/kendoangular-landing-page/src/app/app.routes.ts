82e248950882e5090fc798c4a333b4d25e4ab400
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2025-04-25 12:03:20 +0300

    chore: update routing

--- examples-standalone/kendoangular-landing-page/src/app/app.routes.ts
+++ examples-standalone/kendoangular-landing-page/src/app/app.routes.ts
@@ -1,15 +1,43 @@
-import { Routes } from '@angular/router';
-import { TransactionsDashboardComponent } from './components/header/transactions-dashboard/transactions-dashboard.component';
-import { BottomLeftComponent } from './components/header/bottom-left/bottom-left.component';
-import { BottomRightComponent } from './components/header/bottom-right/bottom-right.component';
-import { DynamicGridComponent } from './components/dynamic-grid/dynamic-grid.component';
+import { Routes } from "@angular/router";
 
 export const routes: Routes = [
-    { path: 'dashboard', component: TransactionsDashboardComponent },
-    { path: 'transactions', component: TransactionsDashboardComponent },
-    { path: 'bottom-left', component: BottomLeftComponent },
-    { path: 'bottom-right', component: BottomRightComponent },
-    { path: 'grid', component: DynamicGridComponent },
-    { path: 'home', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    {
+        path: "grid",
+        loadComponent: () =>
+            import("./components/dynamic-grid/dynamic-grid.component").then((m) => m.DynamicGridComponent),
+    },
+    {
+        path: "scheduler",
+        loadComponent: () => import("./components/scheduler/scheduler.component").then((m) => m.SchedulerComponent),
+    },
+    {
+        path: "charts",
+        loadComponent: () => import("./components/charts/charts.component").then((m) => m.ChartsComponent),
+    },
+    {
+        path: "header",
+        loadComponent: () => import("./components/header/header.component").then((m) => m.HeaderComponent),
+    },
+    {
+        path: "dateinputs",
+        loadComponent: () =>
+            import("./components/date-inputs/date-inputs.component").then((m) => m.DateInputsComponent),
+    },
+    {
+        path: "dropdowns",
+        loadComponent: () => import("./components/dropdowns/dropdowns.component").then((m) => m.DropdownsComponent),
+    },
+    {
+        path: "layout",
+        loadComponent: () => import("./components/layout/my-layout.component").then((m) => m.MyLayoutComponent),
+    },
+    {
+        path: "chat",
+        loadComponent: () =>
+            import("./components/conversational-ui/conversational-ui.component").then(
+                (m) => m.ConversationalUiComponent
+            ),
+    },
+    { path: "home", redirectTo: "header", pathMatch: "full" },
+    { path: "", redirectTo: "header", pathMatch: "full" },
 ];
--- examples-standalone/kendoangular-landing-page/src/main.ts
+++ examples-standalone/kendoangular-landing-page/src/main.ts
@@ -1,8 +1,18 @@
-/// <reference types="@angular/localize" />
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withHashLocation()
+    ),
+    { provide: APP_BASE_HREF, useValue: '/overview/' },
+    provideAnimations()
+  ]
+}).catch(err => console.error(err));
