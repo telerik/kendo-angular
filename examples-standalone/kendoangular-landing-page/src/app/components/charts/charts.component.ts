6a6a0ad1d87044703fe203279a414973694c86d9
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2025-04-25 14:49:16 +0300

    chore: update chart component

--- examples-standalone/kendoangular-landing-page/src/app/components/charts/charts.component.ts
+++ examples-standalone/kendoangular-landing-page/src/app/components/charts/charts.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
-import { KENDO_CHARTS, SeriesLabelsContentArgs } from "@progress/kendo-angular-charts";
-import { commitData } from "../../data/commit-data";
+import { KENDO_CHARTS, Series, SeriesLabelsContentArgs, ValueAxis } from "@progress/kendo-angular-charts";
 
 @Component({
     selector: "app-charts",
@@ -18,16 +17,66 @@ export class ChartsComponent {
         { kind: "Other", share: 0.192 },
     ];
 
-    public commitData = commitData();
-
-    public lineChartCategories = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
-    public lineChartData = [
-        [123, 276, 310, 212, 240, 156, 98],
-        [165, 210, 287, 144, 190, 167, 212],
-        [56, 140, 195, 46, 123, 78, 95],
+    public series: Series[] = [
+        {
+            type: "column",
+            data: [20, 40, 45, 30, 50],
+            stack: true,
+            name: "on battery",
+            color: "#cc6e38",
+        },
+        {
+            type: "column",
+            data: [20, 30, 35, 35, 40],
+            stack: true,
+            name: "on gas",
+            color: "#ef955f",
+        },
+        {
+            type: "line",
+            data: [30, 38, 40, 32, 42],
+            name: "mpg",
+            color: "#ec5e0a",
+            axis: "mpg",
+        },
+        {
+            type: "line",
+            data: [7.8, 6.2, 5.9, 7.4, 5.6],
+            name: "l/100 km",
+            color: "#4e4141",
+            axis: "l100km",
+        },
     ];
+
+    public valueAxes: ValueAxis[] = [
+        {
+            title: { text: "miles" },
+            min: 0,
+            max: 100,
+        },
+        {
+            name: "km",
+            title: { text: "km" },
+            min: 0,
+            max: 161,
+            majorUnit: 32,
+        },
+        {
+            name: "mpg",
+            title: { text: "miles per gallon" },
+            color: "#ec5e0a",
+        },
+        {
+            name: "l100km",
+            title: { text: "liters per 100km" },
+            color: "#4e4141",
+        },
+    ];
+
+    public categories: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 
     public labelContent(e: SeriesLabelsContentArgs): string {
         return e.category;
-    }
+      }
+    public crossingValues: number[] = [0, 0, 10, 10];
 }
