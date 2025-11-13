c8bae672be507a8463257517a504dcbb7166e919
Zornitsa Stoyanova <zstoyano@progress.com>
2025-11-13 17:55:35 +0200

    chore: update versions in kendoangular landing page

--- examples-standalone/kendoangular-landing-page/src/main.ts
+++ examples-standalone/kendoangular-landing-page/src/main.ts
@@ -5,11 +5,13 @@ import { provideAnimations } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app/app.component";
 import { routes } from "./app/app.routes";
+import { provideHttpClient } from "@angular/common/http";
 
 bootstrapApplication(AppComponent, {
     providers: [
         provideRouter(routes, withHashLocation()),
         { provide: APP_BASE_HREF, useValue: "/overview/" },
+        provideHttpClient(),
         provideAnimations(),
     ],
 }).catch((err) => console.error(err));
