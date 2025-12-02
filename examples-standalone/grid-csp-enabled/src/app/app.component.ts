925252a971f41d9f9b72c264adceb831af52e451
mbechev <martinbechev@gmail.com>
2025-12-02 12:43:03 +0200

    chore: update to Angular 21

--- examples-standalone/grid-csp-enabled/src/app/app.component.ts
+++ examples-standalone/grid-csp-enabled/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { GridModule } from '@progress/kendo-angular-grid';
 import { customers } from './customers';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, GridModule],
+    imports: [GridModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
