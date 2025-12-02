f6b5c00f0fab502696813bc144fd2a4e6f5e6c1a
mbechev <martinbechev@gmail.com>
2025-12-02 14:19:11 +0200

    chore: update Angular from v20 to v21

--- examples-standalone/finance-portfolio/src/main.ts
+++ examples-standalone/finance-portfolio/src/main.ts
@@ -1,6 +1,6 @@
 /// <reference types="@angular/localize" />
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -11,5 +11,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-    .bootstrapModule(AppModule)
+    .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
     .catch((err) => console.error(err));
