f779e6507930f1d5eae70c69f1b64e27203fe83c
Martin Bechev <33104481+mbechev@users.noreply.github.com>
2025-12-03 16:49:40 +0200

    chore: update grid live data demo versions

--- examples-standalone/grid-live-data/src/main.ts
+++ examples-standalone/grid-live-data/src/main.ts
@@ -1,8 +1,8 @@
 /// <reference types="@angular/localize" />
-
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
   .catch((err) => console.error(err));
