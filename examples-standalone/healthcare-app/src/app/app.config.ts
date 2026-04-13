b4bc5c462fd15e1da5de8fca11fba9e0a14548c5
mbechev <martinbechev@gmail.com>
2026-04-13 13:31:14 +0300

    chore: redirect to home page

--- examples-standalone/healthcare-app/src/app/app.config.ts
+++ examples-standalone/healthcare-app/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation, withInMemoryScrolling, withRouterConfig } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
@@ -9,7 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
-      withHashLocation(),
       withInMemoryScrolling({
         scrollPositionRestoration: 'top',
       }),
